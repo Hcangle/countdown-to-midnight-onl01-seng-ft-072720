3cb009096c38edd0fc7fcc4bbdea35103b24f5c8
Hannah Angle <hannahcangle@gmail.com>
2020-07-30 04:29:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 
 
 
-def countdown(number)
-  while number > 0 
-   puts "#{number} SECOND(S)!"
-   sleep 1 
-   number -= 1 
+def countdown(x)
+  while x > 0 
+   puts "#{x} SECOND(S)!"
+   sleep (1) 
+   x -= 1 
   end
  return "HAPPY NEW YEAR!"
 end
